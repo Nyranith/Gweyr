eec7f9c20e229e2940d2c0cf830c40f85b30302f
Nyranith <kaalsaas@hotmail.com>
2018-07-03 20:57:40 +0200

    no message

---
+++ Gweyr.PropertyWriter/PropertyWriterModule.cs
@@ -0,0 +1,26 @@
+﻿using Gweyr.PropertyWriter.Views;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace Gweyr.PropertyWriter
+{
+    public class PropertyWriterModule : IModule
+    {
+        private IRegionManager _regionManager;
+        private IUnityContainer _container;
+
+        public PropertyWriterModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _container.RegisterTypeForNavigation<ViewA>();
+        }
+    }
+}
