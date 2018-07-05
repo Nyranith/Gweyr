cb62e31db228ba74e2e9cb9f2cad109595183861
Nyranith <kaalsaas@hotmail.com>
2018-07-05 07:11:35 +0200

    Fixed up in the window, now it copies to clipboard when you create the property.

---
+++ Gweyr.Common/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gweyr.Common
+{
+    public static class StringExtension
+    {
+        public static string FirstToUpper(this string str)
+        {
+            if (String.IsNullOrEmpty(str) || Char.IsLower(str, 0))
+                return str;
+
+            return Char.ToUpperInvariant(str[0]) + str.Substring(1).ToLower();
+        }
+    }
+}
--- Gweyr.PropertyWriter/ViewModels/ViewPropertyWriterViewModel.cs
+++ Gweyr.PropertyWriter/ViewModels/ViewPropertyWriterViewModel.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using Gweyr.Common; 
 
 namespace Gweyr.PropertyWriter.ViewModels
 {
@@ -15,14 +16,90 @@ namespace Gweyr.PropertyWriter.ViewModels
 
         public DelegateCommand<string> TextChangedCommand { get; private set; }
 
-        public ViewPropertyWriterViewModel()
+        public DelegateCommand OnPressCommand { get; private set; }
+
+        private string _propertyType; 
+
+        public string PropertyType
         {
-            TextChangedCommand = new DelegateCommand<string>(TextChanged);
+            get
+            {
+                return _propertyType;
+            }
+            set
+            {
+                SetProperty(ref _propertyType, value); 
+            }
         }
 
-        public void TextChanged(string value)
+        private string _name; 
+
+        public string Name
         {
-            Console.WriteLine(value); 
+            get
+            {
+                return _name; 
+            } 
+            set
+            {
+                SetProperty(ref _name, value); 
+            }
+        }
+
+        private string _textBox;
+
+        public string TextBox
+        {
+            get
+            {
+                return _textBox; 
+            }
+            set
+            {
+                SetProperty(ref _textBox, value); 
+            }
+        }
+
+        public ViewPropertyWriterViewModel()
+        {
+            OnPressCommand = new DelegateCommand(CreatePropery); 
+        }
+
+        public void CreatePropery()
+        {
+            var name = Name.FirstToUpper();
+            var privateName = '_' + Name.ToLower();
+            
+            var sb = new StringBuilder();
+            sb.Append("private " + PropertyType + " " + privateName + ";");
+            sb.Append(Environment.NewLine);
+            sb.Append("public " + PropertyType + " " + name);
+            sb.Append('{');
+            sb.AppendLine();
+            sb.Append("get{ ");
+            sb.AppendLine();
+            sb.Append("return " + privateName + ";");
+            sb.AppendLine();
+            sb.Append(" }");
+            sb.AppendLine();
+            sb.Append("set{ ");
+            sb.AppendLine();
+            sb.Append("if(" + privateName + " != value){");
+            sb.AppendLine();
+            sb.Append(privateName + "= value;");
+            sb.AppendLine();
+            sb.Append("OnPropertyChanged(" + '"' + name + '"' + ");");
+            sb.AppendLine();
+            sb.Append('}');
+            sb.AppendLine();
+            sb.Append('}');
+            sb.AppendLine();
+            sb.Append('}');
+            sb.AppendLine();
+
+            
+            TextBox = sb.ToString();
+            Clipboard.SetText(sb.ToString());
         }
 
     }
